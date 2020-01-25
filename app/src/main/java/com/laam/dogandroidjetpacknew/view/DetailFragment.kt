96335724ac51d2313716dad8143606e1a2c5f22b
luthfi <luthfiarifin0222@gmail.com>
2020-01-26 00:04:23 +0700

    Build the menu

--- app/src/main/java/com/laam/dogandroidjetpacknew/view/DetailFragment.kt
+++ app/src/main/java/com/laam/dogandroidjetpacknew/view/DetailFragment.kt
@@ -4,10 +4,8 @@ package com.laam.dogandroidjetpacknew.view
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -38,6 +36,7 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
         return dataBinding.root
 //        return inflater.inflate(R.layout.fragment_detail, container, false)
@@ -86,5 +85,22 @@ class DetailFragment : Fragment() {
                 }
 
             })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.detail_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_send_sms -> {
+
+            }
+            R.id.action_share -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
